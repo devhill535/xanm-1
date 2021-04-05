004e126460c8997c5b164d38374b9edec0cb0230
devhill535 <77030591+devhill535@users.noreply.github.com>
2021-04-05 19:51:16 +0300

    Update help.js

--- others/help.js
+++ others/help.js
@@ -1,43 +1,50 @@
-const { Client, Collection, MessageEmbed } = require(`discord.js`);
+const { MessageEmbed } = require("discord.js");
 
 module.exports = {
   name: "help",
   aliases: ["h"],
-  description: "HELP COMMANDS",
+  cooldown: 8,
+  description: "**all commands**",
   execute(message) {
     let commands = message.client.commands.array();
     let helpEmbed = new MessageEmbed()
-      .setAuthor(`Help BOT`)
-      .setThumbnail(message.author.avatarURL({dynamic: "true"}))
-      .setFooter(message.author.username,message.author.displayAvatarURL({ dynamic: "true" }))
-      .setColor("RANDOM")
-      
-    .setDescription(`\`
+    .setAuthor("Help Commands!","https://cdn.discordapp.com/emojis/775437938142085170.gif?v=1&size=64")
+    .setImage('https://media.discordapp.net/attachments/807301710237270026/808391933759455282/image0.gif')
+    .setTitle(`**SONG BOT**`)
+    .setThumbnail("")
+    .setAuthor("Prefix [ $ ]","")
+    .setDescription(` 
+\`Help Commands\`
 
-INFO COMMANDS 
+**Music Command**
 
-help, ping, uptime,
+**| x!paly | x!stop | x!skip | x!skipto | x!volume |**
 
-MUSIC COMMANDS
+**| x!queue | x!loop | x!pause | x!nowplaying |**
 
-loop, lyricns ,remove
-,nowplaying, pause,play
-queue, radio ,skipto 
-resume,search, shuffle
-skip, stop, volume
+**| x!shuffle | x!uptime | x!search | x!remove |**
 
-FILTERS COMMANDS 
+**| x!clip | x!clips | x!lyrics | x!move | x!playlist |**
 
-bassboost ,8D ,vaporwave 
-,nightcore ,phaser ,tremolo
-vibrato ,surrounding ,pulsator 
-,subboost ,clear
+**Music Easy Commands**
 
-\`
-**[   SUPPORT  ](https://discord.gg/VehtKMcb2N)** -  **[   INVITE   ](https://discord.com/api/oauth2/authorize?client_id=828649540113661962&permissions=8&scope=bot)** 
+**| x!p | x!s | x!sk | x!st | x!v | x!q | x!l | x!pa | x!np |**
 
-    //////////////
-   //////////////
+**| x!sh | x!u | x!se | x!rm | x!r | x!ms | x!b | x!sp |**
+
+**| x!c | x!cl | x!pl | x!mv | x!ly | x!add | x!h |**
+     
+**[ INVITE ]**
+**[ click here ](https://discord.com/api/oauth2/authorize?client_id=828649540113661962&permissions=8&scope=bot)** 
+**[ SUPPORT BOT  ]**
+**[ click here ](https://discord.com/api/oauth2/authorize?client_id=733469607175913593&permissions=8&scope=bot)**                                                                
+
+`)
+    
+    helpEmbed.setTimestamp();
+
     return message.channel.send(helpEmbed).catch(console.error);
+
   }
 };
+
